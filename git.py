e4c2bf796a0b52c3bdb914540b1d73ac02b2473e
hyvy <hyvy62@gmail.com>
2019-07-31 23:14:24 +0900

    Crawler Update : 2019-07-31 23:14:24.270397

--- git.py
+++ git.py
@@ -5,7 +5,7 @@ import datetime
 dir = '/workspace/adviwith/main' 
 
 dt = datetime.datetime.now()
-subprocess.call("git pull", cwd = dir, shell = True)
+# subprocess.call("git pull", cwd = dir, shell = True)
 subprocess.call("git add .", cwd = dir, shell = True)
 subprocess.call('git commit -m "Crawler Update : {0}"'.format(dt), cwd = dir, shell = True)
 subprocess.call("git push", cwd = dir, shell = True)
