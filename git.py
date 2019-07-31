d566c6a60d2dc3f769f570498d1fc5aa9b80d8d0
hyvy <hyvy62@gmail.com>
2019-07-31 22:08:22 +0900

    2019.07.31 &H:&M:&S : crawler update

--- git.py
+++ git.py
@@ -4,6 +4,11 @@ import datetime
 
 dt = datetime.datetime.now()
 date = dt.strftime("%Y.%m.%d &H:&M:&S")
-subprocess.call("git add .", shell = True)
-subprocess.call('git commit -m "{0} : crawler update"'.format(date), shell = True)
-subprocess.call("git push", shell = True)
+subprocess.call("git add .", cwd = '/workspace/adviwith/main', shell = True)
+subprocess.call('git commit -m "{0} : crawler update"'.format(date), cwd = '/workspace/adviwith/main', shell = True)
+subprocess.call("git push", input="adviwith\nadvi0101\n", cwd = '/workspace/adviwith/main', shell = True)
+# subprocess.run(["git", "add", "."], cwd = '/workspace/adviwith/main', shell = True)
+# subprocess.run(["git", "commit", "-m", '"123"'], cwd = '/workspace/adviwith/main', shell = True)
+# subprocess.run(["git","push"], cwd = '/workspace/adviwith/main', shell = True)
+
+# subprocess.call('ls', cwd = '/workspace/adviwith/main', shell = True)
